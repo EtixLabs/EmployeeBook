92b02dcca0958c6f5fe0d86afd0c78a103fd9174
Anas AIT ALI <aitali.anas@gmail.com>
2015-12-20 18:34:00 +0000

    Fix profilLocation not commit

--- client/app/profil/profil.js
+++ client/app/profil/profil.js
@@ -1,9 +1,8 @@
 'use strict';
 
 angular.module('etixbookApp')
-.config(function($stateProvider) {
-  $stateProvider
-  .state('profil', {
+.config(function ($stateProvider) {
+  $stateProvider.state('profil', {
     url: '/:slug',
     templateUrl: 'app/profil/profil.html',
     authenticate: false,
@@ -11,14 +10,31 @@ angular.module('etixbookApp')
     controllerAs: 'profilCtrl'
   });
 })
-.filter('ageFilter', function() {
+.filter('ageFilter', function () {
   function calculateAge(birthday) {
     var birthday = new Date(birthday).getTime();
     var ageDifMs = Date.now() - birthday;
     var ageDate = new Date(ageDifMs);
     return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
- return function(birthdate) { 
-   return calculateAge(birthdate);
- }; 
+  return function (birthdate) {
+    return calculateAge(birthdate);
+  };
+})
+.filter('profilLocation', function () {
+  return function (location) {
+    var fullLocation = "";
+    switch (location) {
+      case "lu":
+      fullLocation = "Luxembourg";
+      break;
+      default:
+        if (location) {
+          console.log("This location : "+location+" should be handled");
+        }
+      return location;
+    }
+    return fullLocation;
+  };
 });
+//# sourceMappingURL=profil.js.map
